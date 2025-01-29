e19bdb2fd7cb1404170af1fe68657031b27b53ed
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-01-30 00:53:33 +0530

    Authentication Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Providers from "@/components/providers";
+import NextProviders from "@/components/nextProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <NextProviders>{children}</NextProviders>
+        </Providers>
       </body>
     </html>
   );
